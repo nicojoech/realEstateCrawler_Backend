285ce70f3c5a413d88bd38d8ec8eab37e48afc74
David <david281100@hotmail.com>
2023-12-06 16:26:20 +0100

    added proper number formating (area, number of rooms and price)

--- webcrawler/extractor.py
+++ webcrawler/extractor.py
@@ -32,6 +32,30 @@ class Extractor:
 
         return element.get_text(strip=True) if element else None
 
+    @staticmethod
+    def _extract_numeric_part(text):
+        """
+        Extracts the numeric part from the provided text.
+        """
+        if text:
+            # Check if the text starts with '€' and extract the numeric part and convert to flaot
+            if text.strip().startswith('€'):
+                # Regex pattern: € followed by 0 or more spaces followed by 1 or more digits can have a comma as decimal
+                # separator followed by 0 or more digits
+                match = re.search(r'€\s*(\d+(?:,\d+)?)', text)
+                if match:
+                    # Replace comma with dot for decimal
+                    num = float(match.group(1).replace(',', '.'))
+                    # Format to 2 decimal places
+                    return "{:.2f}".format(num)
+
+            # For other cases, extract the numeric part and convert to int
+            else:
+                match = re.search(r'\d+(\.\d+)?', text)
+                if match:
+                    return int(match.group())
+        return None
+
     def extract_data(self):
         # Debugging line
         # print(self.soup.prettify())
@@ -52,10 +76,17 @@ class Extractor:
                 'link': entry.get('href'),
                 'title': self._extract_text(entry, tag_name='h3'),
                 'address': self._extract_text(entry, tag_name='span', attrs={'aria-label': True}),
-                'area': self._extract_text(entry, pattern=area_pattern),
-                'number_of_rooms': self._extract_text(entry, pattern=rooms_pattern),
+
+                'area': self._extract_numeric_part(
+                    self._extract_text(entry, pattern=area_pattern)
+                ),
+                'number_of_rooms': self._extract_numeric_part(
+                    self._extract_text(entry, pattern=rooms_pattern)
+                ),
                 'additional_info': self._extract_text(entry, pattern=additional_info_pattern),
-                'price': self._extract_text(entry, css_selector='span[data-testid^="search-result-entry-price"]')
+                'price': self._extract_numeric_part(
+                    self._extract_text(entry, css_selector='span[data-testid^="search-result-entry-price"]')
+                )
             }
             entries_list.append(entry_data)
 
