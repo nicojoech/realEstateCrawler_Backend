c557bcbc47c649cb1681066b2b47a1266b893503
David <david281100@hotmail.com>
2023-12-17 17:16:59 +0100

    config file extension to check for env file and variables

--- app/services/emailService/config.py
+++ app/services/emailService/config.py
@@ -1,14 +1,23 @@
 import os
-from dotenv import load_dotenv
+from dotenv import load_dotenv, find_dotenv
 
-try:
-    load_dotenv()
+# Check if .env file exists
+if not find_dotenv():
+    raise FileNotFoundError("No .env file found. Please create one with the necessary configurations.")
 
-    HOST = os.getenv('EMAIL_HOST')
-    PORT = os.getenv('EMAIL_PORT')
-    SENDER = os.getenv('EMAIL_SENDER')
-    PASSWORD = os.getenv('EMAIL_PASSWORD')
+# Load .env file
+load_dotenv(find_dotenv())
 
-except Exception as e:
-    print(f'Error loading env variables: {e}')
-    exit(1)
+
+def get_env_variable(name):
+    try:
+        return os.environ[name]
+    except KeyError:
+        raise KeyError(f"Environment variable {name} not found. Ensure the .env file is set up correctly.")
+
+
+# Get environment variables
+EMAIL_SENDER = get_env_variable("EMAIL_SENDER")
+EMAIL_PASSWORD = get_env_variable("EMAIL_PASSWORD")
+SMTP_SERVER = get_env_variable("SMTP_SERVER")
+SMTP_PORT = get_env_variable("SMTP_PORT")
