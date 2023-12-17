9ba9dbd9b80ade1365ab7ad5d36815e211ce9be4
David <david281100@hotmail.com>
2023-12-17 17:06:25 +0100

    config.py file to extract environment variables for emailService

--- app/services/emailService/config.py
+++ app/services/emailService/config.py
@@ -1,2 +1,14 @@
 import os
 from dotenv import load_dotenv
+
+try:
+    load_dotenv()
+
+    HOST = os.getenv('EMAIL_HOST')
+    PORT = os.getenv('EMAIL_PORT')
+    SENDER = os.getenv('EMAIL_SENDER')
+    PASSWORD = os.getenv('EMAIL_PASSWORD')
+
+except Exception as e:
+    print(f'Error loading env variables: {e}')
+    exit(1)
